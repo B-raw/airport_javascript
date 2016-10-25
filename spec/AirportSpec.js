2f046955818cebec34ef0fccad1329f9cd062088
souljuse <oppo.irene@gmail.com>
2016-10-25 11:47:46 +0200

    test for clear for landing added

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -2,13 +2,20 @@
 
 describe('Airport', function(){
   var airport;
+  var plane;
 
   beforeEach(function(){
     airport = new Airport();
+    plane = jasmine.createSpy('plane', ['land']);
   });
 
   it('airport has no planes by default', function(){
     expect(airport.planes()).toEqual([]);
+  });
+
+  it('airport can clear for landing', function(){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
   });
 
 });

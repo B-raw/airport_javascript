c1efab62003b80f7fa47b7895439717025d5d00e
B-raw <michaelbarry1989@hotmail.com>
2016-10-25 10:26:53 +0100

    Add plane test

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -2,13 +2,15 @@
 
 describe('Plane', function(){
   var plane;
+  var airport;
 
   beforeEach(function(){
     plane = new Plane();
+    airport = jasmine.createSpyObj('airport',['clearForLanding']);
   });
 
-  it('planes can land at an airport', function(){
-    expect(plane.land).not.toBeUndefined()
+  it('can land at an airport', function(){
+    plane.land(airport);
+    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
   });
-
 });
